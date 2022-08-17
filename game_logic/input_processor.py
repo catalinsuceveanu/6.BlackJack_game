403ec7f7f86df5991614b9cb303b2e61f546b66e
Catalin <catalinsuceveanu@gmail.com>
2022-08-17 12:21:09 +0300

    update the bet logic to have only 10 tries

--- game_logic/input_processor.py
+++ game_logic/input_processor.py
@@ -13,9 +13,10 @@ def initiate_hand(amount):
 
 
 def place_your_bet(amount):
-    bet = None
-    while True:
+    tries = 10
+    while tries > 0:
         response = input("Place your bet: ")
+        tries -= 1
         try:
             response = int(response)
             if response <= 0:
@@ -23,15 +24,14 @@ def place_your_bet(amount):
             elif response > amount:
                 raise custom_errors.BetHigherThanAmountError()
             else:
-                bet = response
-                break
+                return response
         except ValueError:
             print("Not valid, your bet has to be an integer.")
         except custom_errors.SubZeroBetError:
             print("Not a valid bet, please place a bet higher than $0.")
         except custom_errors.BetHigherThanAmountError:
             print(f"Too high, you only have ${amount}.")
-    return bet
+    print("Ran out of tries, restart the game to play again.")
 
 
 get_a_bet = place_your_bet(500)

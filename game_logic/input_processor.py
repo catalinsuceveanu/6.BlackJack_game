9b6825a6fcfb2b83d158bc16646dd51010e889eb
Catalin <catalinsuceveanu@gmail.com>
2022-08-17 11:15:15 +0300

    create an initiate_hand method

--- game_logic/input_processor.py
+++ game_logic/input_processor.py
@@ -1,3 +1,9 @@
 def initiate_hand(amount):
-    response = input(f"You are starting with ${amount}. Would you like to play a hand?")
-    pass
+    response = input(
+        f"You are starting with ${amount}. Would you like to play a hand? "
+    )
+    acceptable_responses = ("yes", "Yes", "YES", "y", "Y")
+    if response in acceptable_responses:
+        return True
+    else:
+        print("Ok, goodbye!")

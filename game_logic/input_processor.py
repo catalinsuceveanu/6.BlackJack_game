6181164202a85c44a3626ff9b2a29e249fba7f4d
Catalin <catalinsuceveanu@gmail.com>
2022-08-17 12:12:19 +0300

    define a method to get the right input for the bet

--- game_logic/input_processor.py
+++ game_logic/input_processor.py
@@ -1,3 +1,6 @@
+import custom_errors
+
+
 def initiate_hand(amount):
     response = input(
         f"You are starting with ${amount}. Would you like to play a hand? "
@@ -7,3 +10,29 @@ def initiate_hand(amount):
         return True
     else:
         print("Ok, goodbye!")
+
+
+def place_your_bet(amount):
+    bet = None
+    while True:
+        response = input("Place your bet: ")
+        try:
+            response = int(response)
+            if response <= 0:
+                raise custom_errors.SubZeroBetError()
+            elif response > amount:
+                raise custom_errors.BetHigherThanAmountError()
+            else:
+                bet = response
+                break
+        except ValueError:
+            print("Not valid, your bet has to be an integer.")
+        except custom_errors.SubZeroBetError:
+            print("Not a valid bet, please place a bet higher than $0.")
+        except custom_errors.BetHigherThanAmountError:
+            print(f"Too high, you only have ${amount}.")
+    return bet
+
+
+get_a_bet = place_your_bet(500)
+print("Your bet is", get_a_bet)
